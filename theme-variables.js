0086e7318b19034a340527247620bbc47970eb82
Terje Urnes <terje@urnes.net>
2021-08-08 00:14:45 +0200

    Adding background to the content blocks

--- theme-variables.js
+++ theme-variables.js
@@ -5,6 +5,9 @@ var generateThemeVariables = function(customConfig) {
 		:root {
 			--background-color: ${customConfig.backgroundColor};
 			--background-image-opacity: ${customConfig.backgroundPictureOpacity};
+
+			--content-background-color: ${customConfig.contentBackgroundColor};
+			--content-background-blur: ${customConfig.contentBackgroundBlur}px;
 			
 			--base-font-size: ${customConfig.baseFontSize}em;
 			--base-text-color: ${customConfig.textColor};
