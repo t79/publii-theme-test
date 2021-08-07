754e346c91c835b175f2d2999c5a2711af7f2378
Terje Urnes <terje@urnes.net>
2021-08-07 06:30:57 +0200

    Adding font styling

--- theme-variables.js
+++ theme-variables.js
@@ -5,6 +5,9 @@ var generateThemeVariables = function(customConfig) {
 		:root {
 			--background-color: ${customConfig.backgroundColor};
 			--background-image-opacity: ${customConfig.backgroundPictureOpacity};
+			
+			--base-font-size: ${customConfig.baseFontSize}em;
+			--base-text-color: ${customConfig.textColor};
 		}
 	`;
 	
