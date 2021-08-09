057b9ab5cceff73dc3e81447a4b588e40e62f2ba
Terje Urnes <terje@urnes.net>
2021-08-09 18:24:20 +0200

    Setting the backend to send a email.

--- backend/contact-backend.py
+++ backend/contact-backend.py
@@ -1,6 +1,8 @@
 # Code for running on AWS lambda.
 
 import json
+import boto3
+import botocore
 
 HEADERS = {
     'Access-Control-Allow-Origin': 'https://publiitest.t79.it',
@@ -17,9 +19,53 @@ def lambda_handler(event, context):
             'headers': HEADERS,
             'body': json.dumps('The server did not get any name')
         }
-
+        
+    
+    name = body['name']
+    
+    if name == '':
+        name = 'Anonymous'
+    
+    # Get contact with the email service.
+    sesClient = boto3.client('ses')
+    
+    # Try to send email
+    try:
+        response = sesClient.send_email(
+            Source = 'The PubliiTest website <form-publiitest@t79.it>',
+            Destination = {
+                'ToAddresses': [
+                        'PubliiTest owner <owner-publiitest@t79.it>'
+                    ]
+            },
+            Message = {
+                'Subject': {
+                    'Charset': 'UTF-8',
+                    'Data': 'Message from ' + name
+                },
+                'Body': {
+                    'Text': {
+                        'Charset': 'UTF-8',
+                        'Data': 'This is a test'
+                    }
+                }
+            },
+            ReplyToAddresses = [
+                    'no-replay <no-replay@t79.it>'
+                ]
+            )
+            
+    except botocore.exceptions.ClientError as error:
+        print(error)
+        return {
+            'statusCode': 500,
+            'headers': HEADERS,
+            'body': json.dumps('Error: Got failure response from the email service.')
+        }
+    
+    
     return {
         'statusCode': 200,
         'headers': HEADERS,
-        'body': json.dumps('Hi ' +  body['name'] + ', nice to see you.')
+        'body': json.dumps('The email was sent.')
     }
